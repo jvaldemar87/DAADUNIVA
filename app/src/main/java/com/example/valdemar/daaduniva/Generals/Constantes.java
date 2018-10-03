acdd2411153af190b3c9084b009a2d4bbfa45370
jvaldemar87 <jvaldemar87@gmail.com>
2018-10-03 09:07:12 -0500

    actualizado al 03-10-2018
    0906

--- app/src/main/java/com/example/valdemar/daaduniva/Generals/Constantes.java
+++ app/src/main/java/com/example/valdemar/daaduniva/Generals/Constantes.java
@@ -1,5 +1,5 @@
 package com.example.valdemar.daaduniva.Generals;
 
 public class Constantes {
-    public final static String BASE_URL = "http://univa.preguntapp.mx/index.php/mobile/";
+    public final static String BASE_URL = "http://daad.univa.mx/index.php/mobile/";
 }
